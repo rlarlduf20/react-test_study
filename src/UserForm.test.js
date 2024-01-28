3f693be7d8ba77c0c900eb259102e48ff5907c13
rlarlduf20 <kky8274@naver.com>
2024-01-28 22:57:10 +0900

    Test: UserForm 컴포넌트 테스트 코드 작성

---
+++ src/UserForm.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import UserForm from "./UserForm";
+
+test("it shows two inputs and a button", () => {
+  render(<UserForm />);
+
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
