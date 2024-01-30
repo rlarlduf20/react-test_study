a0c777db8d9fd2fdd690000d6d6d995e59d4c4ea
rlarlduf20 <kky8274@naver.com>
2024-01-30 23:11:26 +0900

    Test: UserForm 컴포넌트 테스트 수정
    mocking함수 사용
    label의 텍스트로 input 특정화 시키기

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -13,17 +13,21 @@ test("it shows two inputs and a button", () => {
 });
 
 test("it calls onUserAdd when the form is submitted", async () => {
-  // 방식 1(NOT THE BEST IMPLEMENT)
-
   // 컴포넌트 렌더링하자
-  const argList = [];
-  const callback = (...args) => {
-    argList.push(args);
-  };
-  render(<UserForm onUserAdd={callback} />);
+
+  // 방식 1(NOT THE BEST IMPLEMENT)
+  // const argList = [];
+  // const callback = (...args) => {
+  //   argList.push(args);
+  // };
+  // 방식 2(mocking)
+  const mock = jest.fn();
+  render(<UserForm onUserAdd={mock} />);
 
   // 두개의 input을 찾자(가지고 오자)
-  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  // const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
 
   // name을 입력하는 이벤트
   await user.click(nameInput);
@@ -40,6 +44,8 @@ test("it calls onUserAdd when the form is submitted", async () => {
   await user.click(button);
 
   // onUserAdd 함수가 name과 email과 함께 호출되는지 확인하자
-  expect(argList).toHaveLength(1);
-  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@naver.com" });
+  // expect(argList).toHaveLength(1);
+  // expect(argList[0][0]).toEqual({ name: "jane", email: "jane@naver.com" });
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@naver.com" });
 });
